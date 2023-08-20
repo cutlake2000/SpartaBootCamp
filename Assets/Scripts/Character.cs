d614ec3d3102324e6f4fd43dccb4349447598883
kksoo0131 <a67636521@gmail.com>
2023-08-20 12:51:27 +0900

    GameManager

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    // 게임 매니저에서는 char들을 list로 관리한다.
+
+    // 버튼을 누르면 이번 캐릭터의 isSelected = false가되고
+    // index++ 되면서 list에 해당된 캐릭터들의 isSelected = true가 된다.
+    // 만약 캐릭터의 isSelected가 true라면 캐릭터의 Scale이 커진다.
+
+    // Update문에서 호출하면 계속해서 isSelected를 검사해야하니까
+    // 그냥 버튼에 함수를 연결?
+    public string name;
+
+    private void Awake()
+    {
+
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void SizeUp()
+    {
+        transform.localScale = new Vector3(1.0f, 2.0f, 1);
+    }
+
+    public void DefaultSize()
+    {
+        transform.localScale = new Vector3(0.5f, 1.0f, 1);
+    }
+    
+    
+}
